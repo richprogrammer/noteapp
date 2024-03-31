32e86ea60803be940ee47025c08215f55a40a4d6
Rich <richardleelovestoprogram@gmail.com>
2024-03-31 07:54:23 -0400

    update auth.py

--- website/auth.py
+++ website/auth.py
@@ -23,7 +23,7 @@ def login():
         else:
             flash('Email does not exist.', category='error')
         
-    return render_template('login.html', boolean=True)
+    return render_template('login.html', user=current_user)
 
 @auth.route('/logout')
 @login_required
@@ -58,4 +58,4 @@ def signup():
             flash('Account created!', category='success')
             return redirect(url_for('views.home'))
 
-    return render_template('sign_up.html')
+    return render_template('sign_up.html', user=current_user)
